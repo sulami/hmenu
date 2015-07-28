ddf0caa9dca76ee6857c33169b253d419aa45c3c
Robin Schroer <sulami@peerwire.org>
2015-07-28 17:42:55 +0200

    emulate control-w support

--- hmenu.hs
+++ hmenu.hs
@@ -13,6 +13,9 @@ ff :: [String] -> String -> IO ()
 ff list q = do query <- getChar
                let nq = case query of
                           '\x7f' -> if q == "" then q else init q -- Backspace
+                          '\x17' -> let ws = words q
+                                    in if length ws == 1 then ""
+                                       else unwords (init ws) ++ " " -- Ctrl-W
                           _      -> q ++ [query]
                putChar '\n'
                let results = case nq of
