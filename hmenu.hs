776064532f14d6a889da477c6ae29c9df7999423
Robin Schroer <sulami@peerwire.org>
2015-07-29 01:24:50 +0200

    add actual process calling, first edition

--- hmenu.hs
+++ hmenu.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Data.List (intersperse)
 import System.Environment (getArgs)
+import System.Exit (exitSuccess)
 import System.IO (
   hFlush, stdout, hSetBuffering, stdin, BufferMode (NoBuffering), hSetEcho
   )
+import System.Process (callCommand)
 
 import Files
 import FuzzyFinder
@@ -14,11 +16,15 @@ ff list q = do query <- getChar
                let nq = case query of
                           '\x7f' -> if q == "" then q else init q -- Backspace
                           '\x17' -> ""                            -- Ctrl-w
+                          '\n'   -> if q == "" then "true" else q -- Enter
                           _      -> q ++ [query]
-               putChar '\n'
                let results = case nq of
                               "" -> list
                               _  -> fuzzyFinder nq list
+               if query == '\n'
+                 then do callCommand $ head results
+                         exitSuccess
+                 else putChar '\n'
                mapM_ putStr $ intersperse " " results
                putStr $ "\n> " ++ nq
                hFlush stdout
