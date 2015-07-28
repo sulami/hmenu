b47b7de8de26ba2a3cc5d1fc8bb8adfcb61fc062
Robin Schroer <sulami@peerwire.org>
2015-07-28 15:01:59 +0200

    get the path in the main program, but do not use it yet

--- hmenu.hs
+++ hmenu.hs
@@ -1,10 +1,20 @@
 import Data.List (intersperse)
-import System.Environment (getArgs)
+import System.Environment (getArgs, getEnv)
 import System.IO (hFlush, stdout)
 
 import FuzzyFinder
 
+-- Get $PATH and split the directories properly into a list.
+getPath :: IO [String]
+getPath = fmap split $ getEnv "PATH"
+  where
+    split :: String -> [String]
+    split "" = []
+    split s  = let (a,b) = break (== ':') s
+                in a : split (dropWhile (== ':') b)
+
 main = do list <- getArgs
+          path <- getPath
           putStr "Entery query: "
           hFlush stdout
           query <- getLine
