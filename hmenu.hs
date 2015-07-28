20c8abd372135ebde1999ba065b5e64a8cb9b7da
Robin Schroer <sulami@peerwire.org>
2015-07-28 16:43:18 +0200

    implement a simple interactive promt

--- hmenu.hs
+++ hmenu.hs
@@ -1,14 +1,28 @@
+module Main where
+
 import Data.List (intersperse)
 import System.Environment (getArgs)
-import System.IO (hFlush, stdout)
+import System.IO (
+  hFlush, stdout, hSetBuffering, stdin, BufferMode (NoBuffering)
+  )
 
 import Files
 import FuzzyFinder
 
+ff :: [String] -> String -> IO ()
+ff list q = do query <- getChar
+               let nq = case query of
+                          '\x7f' -> if q == "" then q else init q -- Backspace
+                          _      -> q ++ [query]
+               putChar '\r'
+               case nq of
+                 "" -> putStrLn $ "> " ++ nq
+                 _  -> do mapM_ putStrLn $ fuzzyFinder nq list
+                          putStrLn $ "> " ++ nq
+               ff list nq
+
 main = do path <- getPath
           list <- getExecutables path
-          putStr "Entery query: "
-          hFlush stdout
-          query <- getLine
-          mapM_ putStrLn $ fuzzyFinder query list
+          hSetBuffering stdin NoBuffering
+          ff list ""
 
