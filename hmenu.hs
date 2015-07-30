fede1bcb185ce80287e3a0f67ed36e5f5752f940
Robin Schroer <sulami@peerwire.org>
2015-07-30 13:27:31 +0200

    add cabal; add missingH; use it to implement proper forking

--- hmenu.hs
+++ hmenu.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Data.List (intersperse)
+import System.Cmd.Utils (forkRawSystem)
 import System.Environment (getArgs)
 import System.Exit (exitSuccess)
 import System.IO (
   hFlush, stdout, hSetBuffering, stdin, BufferMode (NoBuffering), hSetEcho
   )
-import System.Process (callCommand)
 
 import Files
 import FuzzyFinder
@@ -22,7 +22,7 @@ ff list q = do query <- getChar
                               "" -> list
                               _  -> fuzzyFinder nq list
                if query == '\n'
-                 then do callCommand $ head results
+                 then do forkRawSystem (head results) []
                          exitSuccess
                  else putChar '\n'
                mapM_ putStr $ intersperse " " results
